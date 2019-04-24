5f0bd3a8663e85fdb8c94f1fe75188fbfcf18469
Mateusz Harasimowicz <mateusz.harasimowicz1@gmail.com>
2019-04-24 20:38:58 +0200

    WebConfig+Handler

---
+++ ShopProject/Handler.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="Handler1.ashx.cs" Class="PS3_WWW.Handler1" %>
---
+++ ShopProject/Handler.ashx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+
+namespace ShopProject
+{
+    public class Handler : IHttpHandler
+    {
+    
+        
+            public void ProcessRequest(HttpContext context)
+            {
+                context.Response.Clear();
+                context.Response.ContentType = "image/jpeg";
+                if (File.Exists(context.Request.PhysicalPath))
+                {
+                    Image image = Image.FromFile(context.Request.PhysicalPath);
+                    Image watermark = Image.FromFile(@"C:/Users/KRS/Desktop/PS3 WWW/PS3 WWW/Images/WaterMark.png");
+                    Graphics g = Graphics.FromImage(image);
+                    float w = (image.Width / 2);
+                    float h = (w / watermark.Width) * watermark.Height;
+                    g.DrawImage(watermark, image.Width / 2 - w / 2, image.Height / 2 - h / 2, w, h);
+                    using (MemoryStream mStream = new MemoryStream())
+                    {
+                        image.Save(mStream, image.RawFormat);
+                        mStream.WriteTo(context.Response.OutputStream);
+                    }
+                }
+                else
+                {
+                    context.Response.ContentType = "text/plain";
+                    context.Response.Write("Bladbladblad!!!");
+                }
+                context.Response.End();
+            }
+
+
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
