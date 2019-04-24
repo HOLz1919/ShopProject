43978b46c62fa92cb91eac000626852b44e76943
Mateusz Harasimowicz <mateusz.harasimowicz1@gmail.com>
2019-04-24 20:53:33 +0200

    Logic Application Start

---
+++ ShopProject/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ShopProject.Models
+{
+    public class User
+    {
+        [Key]
+        public int userId { get; set; }
+
+        [Display(Name = "Nazwa uzytkownika")]
+        public string userName { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string password { get; set; }
+
+        [Display(Name = "Imię")]
+        public string firstName { get; set; }
+
+        [Display(Name = "Nazwisko")]
+        public string lastName { get; set; }
+
+        [Display(Name = "Wiek")]
+        public int age { get; set; }
+
+
+    }
+}
